58df19abd7d48686d66230cbdd76590f9a43f9eb
KaanUzun <kaanuzun.ku@outlook.com>
2019-11-20 03:17:24 +0100

    First

---
+++ src/com/ku/springdemo/FootballCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Ýþte bu Baþardýn!!! Good Job";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
